0e4dac8e934941a7d6c6a16acf08b86dac4743fe
Adrián Vázquez <advzq@pm.me>
2026-04-18 20:28:48 +0200

    started bg-list logic

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // Gestor de eventos
 const inputChevron = document.querySelectorAll(".input-chevron");
+const boardgameSelector = document.querySelectorAll(".bg-list");
 
 const eventManager = {
   handleEvent: function (ev) {
     if (ev.type === "click") {
       ev.currentTarget.classList.toggle("rotate-chevron");
+      ev.boardgameSelector.classList.toggle("hidden");
     }
   },
 };
